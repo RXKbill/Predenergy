74ac1b62b02c218ce4c66dc9af3a33d5bbe71e83
IDLSCTS <147295748+RXKbill@users.noreply.github.com>
2025-07-29 23:24:46 +0800

    update models finish

---
+++ Predenergy/STDM/TS_Lib/__init__.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+__all__ = [
+    "Autoformer",
+    "Crossformer",
+    "DLinear",
+    "ETSformer",
+    "FEDformer",
+    "FiLM",
+    "Informer",
+    "iTransformer",
+    "Koopa",
+    "LightTS",
+    "Linear",
+    "MICN",
+    "NLinear",
+    "Nonstationary_Transformer",
+    "PatchTST",
+    "Pyraformer",
+    "Reformer",
+    "TimesNet",
+    "Transformer",
+    "Triformer",
+    "TimeMixer",
+]
+
+
+from STDM.TS_Lib.models.Autoformer import Autoformer
+from STDM.TS_Lib.models.Crossformer import Crossformer
+from STDM.TS_Lib.models.DLinear import DLinear
+from STDM.TS_Lib.models.ETSformer import ETSformer
+from STDM.TS_Lib.models.FEDformer import FEDformer
+from STDM.TS_Lib.models.FiLM import FiLM
+from STDM.TS_Lib.models.Informer import Informer
+from STDM.TS_Lib.models.iTransformer import iTransformer
+from STDM.TS_Lib.models.Koopa import Koopa
+from STDM.TS_Lib.models.LightTS import LightTS
+from STDM.TS_Lib.patchs.Linear import Linear
+from STDM.TS_Lib.models.MICN import MICN
+from STDM.TS_Lib.patchs.NLinear import NLinear
+from STDM.TS_Lib.models.Nonstationary_Transformer import (
+    Nonstationary_Transformer,
+)
+from STDM.TS_Lib.models.PatchTST import PatchTST
+from STDM.TS_Lib.models.Pyraformer import Pyraformer
+from STDM.TS_Lib.models.Reformer import Reformer
+from STDM.TS_Lib.models.TimesNet import TimesNet
+from STDM.TS_Lib.models.Transformer import Transformer
+from STDM.TS_Lib.patchs.Triformer import Triformer
+from STDM.TS_Lib.models.TimeMixer import TimeMixer
